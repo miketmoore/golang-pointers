3776c65ded8c9c06b288e208d1753a6451acd985
Michael Moore <michael.moore@contrastsecurity.com>
2018-01-31 22:24:37 -0500

    Add documentation

--- pointers.go
+++ pointers.go
@@ -1,17 +1,22 @@
 package pointers
 
+// Container is a struct that encapsulates a slice of strings
+// and exposes various functions for manipulating the slice
 type Container struct {
 	things []string
 }
 
+// New creates an instance of the Container struct and adds a slice of strings
 func New(things []string) Container {
 	return Container{things: things}
 }
 
+// Things returns a pointer to the slice of strings
 func (o *Container) Things() *[]string {
 	return &o.things
 }
 
+// Pop removes the last value from the internal slice of strings and returns this value
 func (o *Container) Pop() string {
 	things := o.things
 	l := len(things)
@@ -20,14 +25,17 @@ func (o *Container) Pop() string {
 	return thing
 }
 
-func (o *Container) Append(s ...string) {
+// Push adds a string to the end of the internal slice of strings
+func (o *Container) Push(s ...string) {
 	o.things = append(o.things, s...)
 }
 
+// SetThings updates the internal slice of strings
 func (o *Container) SetThings(things *[]string) {
 	o.things = *things
 }
 
+// Exchange "exchanges" or "swaps" the value located at the specified index, between the two slices
 func Exchange(a *Container, b *Container, indexToExchange int) {
 	aThings := *a.Things()
 	bThings := *b.Things()
